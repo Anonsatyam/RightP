ebcb44dd37b0e39588801804b49a3fb7bf410037
Satyam Kumar <Satyamdeveloper98@gmail.com>
2020-05-15 21:17:25 +0530

    Modal Popup and Alert Box Added

--- js/script.js
+++ js/script.js
@@ -5,11 +5,6 @@ jQuery(function ($) {
         $('nav ul li').hover(function (e) {
             $(this).children('ul').stop(true, false, true).fadeToggle(300);
         });
-        // $('#nav-toggle').click(function () {
-        //     $('ul.nav-dropdown').hide();
-        //     $('ul.nav-dropdown').show();
-            
-        // });
         $('#nav-toggle').on('click', function () {
             this.classList.toggle('active');
         });
@@ -20,9 +15,6 @@ jQuery(function ($) {
             e.preventDefault();
             $('ul.nav-dropdown').toggle();
         });
-        // $('#nav-toggle').click(function () {
-        //     $('.nav-dropdown').toggle();
-        // });
     });
 });
 
@@ -35,4 +27,34 @@ $(document).ready(function () {
             $('#loadMore').fadeOut(1000);
         }
     });
+});
+
+var ctrl = new ScrollMagic.Controller();
+
+// Create scenes in jQuery each() loop
+$("section").each(function (i) {
+    var inner = $(this).find(".inner");
+    var outer = $(this).find(".outer");
+    var tl = new TimelineMax();
+
+    tl.from(outer, 0.25, {
+        scaleX: 0
+    });
+    tl.from(inner, 0.65, {
+        yPercent: 100,
+        ease: Back.easeOut
+    });
+
+    new ScrollMagic.Scene({
+            triggerElement: this,
+            triggerHook: 0.15
+        })
+        .setTween(tl)
+        .addIndicators({
+            colorTrigger: "white",
+            colorStart: "white",
+            colorEnd: "white",
+            indent: 40
+        })
+        .addTo(ctrl);
 });
